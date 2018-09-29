9b71e9bbec017ab933a3497ff8165d4cfdf2433c
cjq17624 <cjq17624@ly.com>
2018-09-29 14:01:06 +0800

    线程

---
+++ src/SelefTestDemo/ThreadDemo/Demo01.java
@@ -0,0 +1,34 @@
+package SelefTestDemo.ThreadDemo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Demo01 {
+    private List names = new ArrayList();
+
+    public static void main(String[] args) {
+        final Demo01 sl = new Demo01();
+        for (int i = 0; i < 2; i++) {
+            new Thread("线程" + i) {
+                public void run() {
+                    sl.add("A");
+                    sl.add("B");
+                    sl.add("C");
+                    sl.printAll();
+                }
+            }.start();
+        }
+    }
+
+    public synchronized void add(String name) {
+        names.add(name);
+    }
+
+    public synchronized void printAll() {
+        for (int i = 0; i < names.size(); i++) {
+            System.out.print(names.get(i) + "");
+        }
+    }
+}
+
